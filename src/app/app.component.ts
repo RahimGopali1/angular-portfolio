31c5b20f7fd5ccd590c8fc9c8c413a2a03b5f31a
RahimGopali1 <grahim18@tbc.edu.np>
2025-01-29 16:56:56 +0545

    20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,22 @@ import { HeaderComponent } from './shared/components/header/app-header.component
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  title = 'my-portfolio';
-  isLoading = true;
-  isHidden = false;
+export class AppComponent {}
+// export class AppComponent implements OnInit {
+//   title = 'my-portfolio';
+//   isLoading = true;
+//   isHidden = false;
 
-  ngOnInit(): void {
-    this.showLoadingScreen();
-  }
+//   ngOnInit(): void {
+//     this.showLoadingScreen();
+//   }
 
-  showLoadingScreen(): void {
-    setTimeout(() => {
-      this.isHidden = true; // Add the `hidden` class to trigger the animation
-      setTimeout(() => {
-        this.isLoading = false; // Remove the loading screen from the DOM
-      }, 500); // Wait for the CSS transition to complete
-    }, 3000); // Simulated loading delay
-  }
-}
+//   showLoadingScreen(): void {
+//     setTimeout(() => {
+//       this.isHidden = true;
+//       setTimeout(() => {
+//         this.isLoading = false;
+//       }, 500);
+//     }, 3000);
+//   }
+// }
