73a1999fbfd04535f724cb7b19eb3e1449ec1115
RahimGopali1 <grahim18@tbc.edu.np>
2025-01-30 17:35:20 +0545

    progress bar resume

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { ProgressBarComponent } from '../../shared/components/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'resume',
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, MatProgressBarModule, ProgressBarComponent],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
